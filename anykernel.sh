0c460ee65cddd264005048fe616f20298be392a7
Muhammad Fauzan <70912946+buglessx@users.noreply.github.com>
2021-03-29 13:34:16 +0700

    ginkgo: Swipe to Dolphin Kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ryzen Kernel by Edwiin Kusuma Jaya @ telegram
+kernel.string=Dolphin Kernel by xzanxz @ telegram
 do.devicecheck=1
 do.modules=1
 do.systemless=0
